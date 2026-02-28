d4fd3be062785d0c8c6bf5cf1f39984f9784bb19
Adidale <adidale@MacBook-Air-de-Adidale.local>
2026-02-28 17:46:17 +0300

    modified home page

--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -7,16 +7,7 @@ export function Welcome() {
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
+            <h1 className="text-4xl font-bold text-center">Welcome to SPRT</h1>
           </div>
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
@@ -24,21 +15,8 @@ export function Welcome() {
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next?
             </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
+            <p>MySQL is gay</p>
+            <p>Python is shit</p>
           </nav>
         </div>
       </div>
